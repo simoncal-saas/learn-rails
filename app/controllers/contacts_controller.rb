a6ef93bbb034ba30ebd9538efb9d4a9e37ed87ec
simoncal-saas <simoncal@yahoo.com>
2017-08-31 14:51:32 -0700

    combine redirect_to & flash alert in ContactsController#create

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -11,8 +11,8 @@ class ContactsController < ApplicationController
       flash[:notice] = "Message sent from #{@contact.name}."
       redirect_to root_path
     else
-      flash[:alert] = "Missing Name, Email or Message."
-      redirect_to new_contact_path
+#      flash[:alert] = "Missing Name, Email or Message."
+      redirect_to new_contact_path, alert: "Missing Name, Email or Message."
     end
   end
 
